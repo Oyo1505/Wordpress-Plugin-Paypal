391bbe8b6e78ce26fddc28959effefd0920bd417
Oyo1505 <rigoulet.henri.pierre@gmail.com>
2016-11-23 10:47:05 +1300

    Add Paypal ID
    Add input area for Paypal ID

--- Paypal-plugin/Paypal-Button-Widget.php
+++ Paypal-plugin/Paypal-Button-Widget.php
@@ -10,26 +10,27 @@
 
 class WP_paypal_widget extends WP_Widget {
 
-	
+
 
 		public function __construct() {
 
 
 			parent::__construct(
 				// Base ID of your widget
-				'WP_paypal_widget', 
+				'WP_paypal_widget',
 				// Widget name will appear in UI
 				__('Paypal Widget'),
 				// Widget Description
-				array('Description' =>  __( 'Sample widget based on WPBeginner Tutorial', 'paypal_widget_domain' ),)
+				array('Description' =>  __( 'Just a Paypal Button, For fun', 'paypal_widget_domain' ),)
 				);
 		}
-	
+
 		//front-end display
 	public function widget($args, $instance){
 
 		$title = apply_filters( 'widget_title', $instance['title']) ;
 		$button = $instance ['button'];
+		//$key = $instance['key']
 		// before and after widget arguments are defined by themes
 
 		echo $args['before_widget'];
@@ -41,8 +42,7 @@ class WP_paypal_widget extends WP_Widget {
 		?>
 			<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
 		            <input type="hidden" name="cmd" value="_s-xclick">
-		            <input type="hidden" name="encrypted" value="-----BEGIN PKCS7-----MIIHNwYJKoZIhvcNAQcEoIIHKDCCByQCAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYBdm/QQYQg/DtKmiHqoly1ip9KsEP2UkeX0eFVDLIxVGxIoO0yXar2ZPruYvBY6kyCDfZWRnrvS20+tMgf2R0IuWQgAwCxya0FHLbvkUa35+3WZntwMKiVpW6fsnn1yTlFU31x5NubxMHz/qQv9ZuZxrVxdMGuTc12KwpZXWUymPjELMAkGBSsOAwIaBQAwgbQGCSqGSIb3DQEHATAUBggqhkiG9w0DBwQIKZZE3tH2F1+AgZCPCUPT5y5wiEJSz8WrIIdrEiY/pwcSjLyLrUOO/xVzqWN/Em9Iz24CigV/Th9xsNSE+He5eaKsjHCog7skCHaWb3LGSY3vkPdNWZJ+BkeXHIq8HWTIJBD9Eks1svs8iQGVOV1oj/J/pC4IEnLAsalXpdHkHmqRNKkFnXvhko3e6DAZyTaz4Cr8FM8sNKTt4BagggOHMIIDgzCCAuygAwIBAgIBADANBgkqhkiG9w0BAQUFADCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20wHhcNMDQwMjEzMTAxMzE1WhcNMzUwMjEzMTAxMzE1WjCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20wgZ8wDQYJKoZIhvcNAQEBBQADgY0AMIGJAoGBAMFHTt38RMxLXJyO2SmS+Ndl72T7oKJ4u4uw+6awntALWh03PewmIJuzbALScsTS4sZoS1fKciBGoh11gIfHzylvkdNe/hJl66/RGqrj5rFb08sAABNTzDTiqqNpJeBsYs/c2aiGozptX2RlnBktH+SUNpAajW724Nv2Wvhif6sFAgMBAAGjge4wgeswHQYDVR0OBBYEFJaffLvGbxe9WT9S1wob7BDWZJRrMIG7BgNVHSMEgbMwgbCAFJaffLvGbxe9WT9S1wob7BDWZJRroYGUpIGRMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbYIBADAMBgNVHRMEBTADAQH/MA0GCSqGSIb3DQEBBQUAA4GBAIFfOlaagFrl71+jq6OKidbWFSE+Q4FqROvdgIONth+8kSK//Y/4ihuE4Ymvzn5ceE3S/iBSQQMjyvb+s2TWbQYDwcp129OPIbD9epdr4tJOUNiSojw7BHwYRiPh58S1xGlFgHFXwrEBb3dgNbMUa+u4qectsMAXpVHnD9wIyfmHMYIBmjCCAZYCAQEwgZQwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tAgEAMAkGBSsOAwIaBQCgXTAYBgkqhkiG9w0BCQMxCwYJKoZIhvcNAQcBMBwGCSqGSIb3DQEJBTEPFw0xNjAzMTAwNjQzMjJaMCMGCSqGSIb3DQEJBDEWBBTCT4ex4qG3Qiup1RDi4/1TlEvP3zANBgkqhkiG9w0BAQEFAASBgDjo5Wbl4sZ+ieXhH2qcJWezkzV52R7chp1MSXrs1AMWuNsa+QpVWncJ3AAvsdfGGg6f4ahp9u7I4732d+pG98wRfXt1uJR1AglfWkG87lFMheEL0MjlJovkqV0rEgUBTdU8In0gpRLEvDVgeBnGLp1gIzwDr1smDxpaX0qahbT1-----END PKCS7-----
-			">
+		        <input type="hidden" name="encrypted" value="-----BEGIN PKCS7----- <?php echo $instance['key'] ?> -----END PKCS7----- ">
             <input type="image" src="https://www.paypalobjects.com/fr_FR/FR/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal, le réflexe sécurité pour payer en ligne">
             <img alt="" border="0" src="https://www.paypalobjects.com/fr_FR/i/scr/pixel.gif" width="1" height="1">
         </form>
@@ -52,8 +52,8 @@ class WP_paypal_widget extends WP_Widget {
 		?>
 		<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
 		            <input type="hidden" name="cmd" value="_s-xclick">
-		            <input type="hidden" name="encrypted" value="-----BEGIN PKCS7-----MIIHNwYJKoZIhvcNAQcEoIIHKDCCByQCAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYBdm/QQYQg/DtKmiHqoly1ip9KsEP2UkeX0eFVDLIxVGxIoO0yXar2ZPruYvBY6kyCDfZWRnrvS20+tMgf2R0IuWQgAwCxya0FHLbvkUa35+3WZntwMKiVpW6fsnn1yTlFU31x5NubxMHz/qQv9ZuZxrVxdMGuTc12KwpZXWUymPjELMAkGBSsOAwIaBQAwgbQGCSqGSIb3DQEHATAUBggqhkiG9w0DBwQIKZZE3tH2F1+AgZCPCUPT5y5wiEJSz8WrIIdrEiY/pwcSjLyLrUOO/xVzqWN/Em9Iz24CigV/Th9xsNSE+He5eaKsjHCog7skCHaWb3LGSY3vkPdNWZJ+BkeXHIq8HWTIJBD9Eks1svs8iQGVOV1oj/J/pC4IEnLAsalXpdHkHmqRNKkFnXvhko3e6DAZyTaz4Cr8FM8sNKTt4BagggOHMIIDgzCCAuygAwIBAgIBADANBgkqhkiG9w0BAQUFADCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20wHhcNMDQwMjEzMTAxMzE1WhcNMzUwMjEzMTAxMzE1WjCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20wgZ8wDQYJKoZIhvcNAQEBBQADgY0AMIGJAoGBAMFHTt38RMxLXJyO2SmS+Ndl72T7oKJ4u4uw+6awntALWh03PewmIJuzbALScsTS4sZoS1fKciBGoh11gIfHzylvkdNe/hJl66/RGqrj5rFb08sAABNTzDTiqqNpJeBsYs/c2aiGozptX2RlnBktH+SUNpAajW724Nv2Wvhif6sFAgMBAAGjge4wgeswHQYDVR0OBBYEFJaffLvGbxe9WT9S1wob7BDWZJRrMIG7BgNVHSMEgbMwgbCAFJaffLvGbxe9WT9S1wob7BDWZJRroYGUpIGRMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbYIBADAMBgNVHRMEBTADAQH/MA0GCSqGSIb3DQEBBQUAA4GBAIFfOlaagFrl71+jq6OKidbWFSE+Q4FqROvdgIONth+8kSK//Y/4ihuE4Ymvzn5ceE3S/iBSQQMjyvb+s2TWbQYDwcp129OPIbD9epdr4tJOUNiSojw7BHwYRiPh58S1xGlFgHFXwrEBb3dgNbMUa+u4qectsMAXpVHnD9wIyfmHMYIBmjCCAZYCAQEwgZQwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tAgEAMAkGBSsOAwIaBQCgXTAYBgkqhkiG9w0BCQMxCwYJKoZIhvcNAQcBMBwGCSqGSIb3DQEJBTEPFw0xNjAzMTAwNjQzMjJaMCMGCSqGSIb3DQEJBDEWBBTCT4ex4qG3Qiup1RDi4/1TlEvP3zANBgkqhkiG9w0BAQEFAASBgDjo5Wbl4sZ+ieXhH2qcJWezkzV52R7chp1MSXrs1AMWuNsa+QpVWncJ3AAvsdfGGg6f4ahp9u7I4732d+pG98wRfXt1uJR1AglfWkG87lFMheEL0MjlJovkqV0rEgUBTdU8In0gpRLEvDVgeBnGLp1gIzwDr1smDxpaX0qahbT1-----END PKCS7-----
-			">
+		            <input type="hidden" name="encrypted" value="-----BEGIN PKCS7----- <?php echo $instance['key'] ?> -----END PKCS7----- ">
+
             <input type="image" src="https://www.paypal.com/fr_FR/FR/i/btn/btn_xpressCheckout.gif" border="0" name="submit" alt="PayPal, le réflexe sécurité pour payer en ligne">
         </form>
 
@@ -63,7 +63,7 @@ class WP_paypal_widget extends WP_Widget {
 		//echo __( 'Paypal Donation ', 'paypal_widget_domain' );
 		echo $args['after_widget'];
 
-			
+
 		}
 
 		// Widget Backend display
@@ -80,7 +80,7 @@ class WP_paypal_widget extends WP_Widget {
 			$title = __( 'New title', 'paypal_widget_domain' );
 
 			}
-		
+
 			/* Stop Adding Functions Below this Line */
 
 			// Widget admin form
@@ -93,19 +93,24 @@ class WP_paypal_widget extends WP_Widget {
 </p>
 
 <p>
-	
+
 		<label for="<?php echo $this->get_field_id( 'button' ); ?>"> <?php _e('Button:');?></label>
 			<select name="<?php echo $this->get_field_name('button'); ?>">
 				<option value="button-1" <?php echo $instance['button'] != 'button-2' ? ' selected="selected"' : ''; ?>><?php _e( 'Button 1', 'paypal_widget_domain'); ?></option>
 				<option value="button-2" <?php echo $instance['button'] == 'button-2' ? ' selected="selected"' : ''; ?>><?php _e( 'Button 2', 'paypal_widget_domain' ); ?></option>
-			
+
 			</select>
-		
+
+</p>
+
+<p>
+	<label for="<?php echo $this->get_field_id('key');?>"> <?php _e('Paypal_ID');?></label>
+	<input type="text" name="<?php echo $this->get_field_name('key'); ?>" value="<?php echo $instance['key']; ?> " >
 </p>
 
 <?php
 }
- 
+
  // Updating widget replacing old instances with new
 
 	public function update($new_instance, $old_instance){
@@ -113,11 +118,11 @@ class WP_paypal_widget extends WP_Widget {
 		$instance = array();
 		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
 		$instance['button']	= strip_tags($new_instance['button'] );
-						
-						
+		$instance['key']	= strip_tags($new_instance['key'] );
+
 
 		return $instance;
-		
+
 	}
 
 }// class paypal_widget end here
